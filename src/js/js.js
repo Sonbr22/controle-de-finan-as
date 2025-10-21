5424fa0ec468eab9a344394ec97ddab493170cfb
Sonbr22 <mayconleite369@gmail.com>
2025-10-21 00:00:20 -0300

    coreção de bug

--- src/js/js.js
+++ src/js/js.js
@@ -1,12 +1,6 @@
-function mostrarSecao(id) {
-  // Seleciona todas as sections e remove a classe "ativo"
-  document.querySelectorAll('section').forEach(secao => {
-    secao.classList.remove('ativo');
-  });
-
-  // Mostra apenas a section clicada
-  const ativa = document.getElementById(id);
-  if (ativa) ativa.classList.add('ativo');
+function mostrarSecao(pagina) {
+  const iframe = document.getElementById('conteudo');
+  if (iframe) {
+    iframe.src = pagina;
+  }
 }
-
-
