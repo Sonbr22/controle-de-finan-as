6969445e656482e2ee7435ccbe0c2db9ba5a52ee
Sonbr22 <mayconleite369@gmail.com>
2025-10-21 13:39:55 -0300

    test

--- src/js/js.js
+++ src/js/js.js
@@ -1,6 +1,10 @@
-function mostrarSecao(pagina) {
-  const iframe = document.getElementById('conteudo');
-  if (iframe) {
-    iframe.src = pagina;
-  }
+function mostrarSecao(id) {
+  // Seleciona todas as sections e remove a classe "ativo"
+  document.querySelectorAll('section').forEach(secao => {
+    secao.classList.remove('ativo');
+  });
+
+  // Mostra apenas a section clicada
+  const ativa = document.getElementById(id);
+  if (ativa) ativa.classList.add('ativo');
 }
